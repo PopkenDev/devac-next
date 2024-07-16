c1c94618bb359c82986fbef468f6fc4464ae0283
PopkenDev <popkendev@outlook.com>
2024-07-17 00:15:19 +0200

    Added showPassword functionality

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,13 +1,24 @@
-"use client";
+'use client';
 
-import { RiUserFill, RiLockFill } from "@remixicon/react";
+import {
+  RiUserFill,
+  RiLockFill,
+  RiEyeFill,
+  RiEyeOffFill,
+} from '@remixicon/react';
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { FormLabel } from "@/components/ui/form-label";
-import { FormItem } from "@/components/ui/form-item";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { FormLabel } from '@/components/ui/form-label';
+import { FormItem } from '@/components/ui/form-item';
+import { useState } from 'react';
 
 export const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormDate] = useState({
+    username: '',
+    password: '',
+  });
   const handleLogin = (event: any) => {
     event.preventDefault();
   };
@@ -17,14 +28,24 @@ export const LoginForm = () => {
         <FormLabel variant="auth" label="Username" />
         <div className="relative">
           <Input type="text" variant="auth" />
-          <RiUserFill className="text-gray-50 w-4 h-4 absolute top-1/2 -translate-y-1/2 left-2" />
+          <RiUserFill className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-50" />
         </div>
       </FormItem>
       <FormItem>
         <FormLabel variant="auth" label="Password" />
         <div className="relative">
-          <Input type="password" variant="auth" />
-          <RiLockFill className="text-gray-50 w-4 h-4 absolute top-1/2 -translate-y-1/2 left-2" />
+          <Input type={showPassword ? 'text' : 'password'} variant="auth" />
+          <RiLockFill className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-50" />
+          <span
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer text-xs font-semibold text-gray-50"
+          >
+            {showPassword ? (
+              <RiEyeOffFill className="h-4 w-4 text-gray-50" />
+            ) : (
+              <RiEyeFill className="h-4 w-4 text-gray-50" />
+            )}
+          </span>
         </div>
       </FormItem>
       <Button label="Login" variant="auth" type="submit" />
--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -1,13 +1,21 @@
 'use client';
 
-import { RiUserFill, RiLockFill, RiMailFill } from '@remixicon/react';
+import {
+  RiUserFill,
+  RiLockFill,
+  RiMailFill,
+  RiEyeOffFill,
+  RiEyeFill,
+} from '@remixicon/react';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { FormLabel } from '@/components/ui/form-label';
 import { FormItem } from '@/components/ui/form-item';
+import { useState } from 'react';
 
 export const RegisterForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
   const handleLogin = (event: any) => {
     event.preventDefault();
   };
@@ -30,15 +38,35 @@ export const RegisterForm = () => {
       <FormItem>
         <FormLabel variant="auth" label="Password" />
         <div className="relative">
-          <Input type="password" variant="auth" />
+          <Input type={showPassword ? 'text' : 'password'} variant="auth" />
           <RiLockFill className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-50" />
+          <span
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer text-xs font-semibold text-gray-50"
+          >
+            {showPassword ? (
+              <RiEyeOffFill className="h-4 w-4 text-gray-50" />
+            ) : (
+              <RiEyeFill className="h-4 w-4 text-gray-50" />
+            )}
+          </span>
         </div>
       </FormItem>
       <FormItem>
         <FormLabel variant="auth" label="Repeat password" />
         <div className="relative">
-          <Input type="password" variant="auth" />
+          <Input type={showPassword ? 'text' : 'password'} variant="auth" />
           <RiLockFill className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-50" />
+          <span
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer text-xs font-semibold text-gray-50"
+          >
+            {showPassword ? (
+              <RiEyeOffFill className="h-4 w-4 text-gray-50" />
+            ) : (
+              <RiEyeFill className="h-4 w-4 text-gray-50" />
+            )}
+          </span>
         </div>
       </FormItem>
       <Button label="Login" variant="auth" type="submit" />
