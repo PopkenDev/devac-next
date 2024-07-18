14056776a6f7628beab5d578e3e6bce3361e0811
Herman Popken <popkendev@outlook.com>
2024-07-18 02:14:53 +0200

    Added leaderbordpage

---
+++ src/app/(app)/leaderboard/page.tsx
@@ -0,0 +1,11 @@
+const LeaderboardPage = () => {
+  return (
+    <main className="h-full">
+      <div className="grid h-full place-items-center bg-white dark:bg-black">
+        <h1 className="text-black dark:text-gray-50">LeaderboardPage</h1>
+      </div>
+    </main>
+  );
+};
+
+export default LeaderboardPage;
