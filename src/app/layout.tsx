820e3a20462ea27f7a44408e74059ee7e8a79672
PopkenDev <popkendev@outlook.com>
2024-07-21 14:32:17 +0200

    minor changes to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import { Lato } from 'next/font/google';
 import './globals.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
 const lato = Lato({
   subsets: ['latin'],
